6c498128dd094e50bbf3e28bfc0c6bf7a5b3d127
EmanuelCAC <emanuelc279@gmail.com>
2024-10-07 22:34:03 -0300

    testing remote access

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [PrismaModule, AuthModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
